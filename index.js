09b155dd52b06c1f6389ed7f93cf4c67e147228e
rubemrochadev <rubemrocha.dev@gmail.com>
2025-05-02 17:04:32 -0300

    CRM

---
+++ index.js
@@ -0,0 +1,15 @@
+function mostrarOverlay() {
+    let overlayElement = document.querySelector("#overlay");
+    let overlayElement.classList.contains("#gaveta");
+
+    if(overlayElement.classList.contains("invisible")){
+        overlayElement.classList.remove("invisible", "opacity-0");
+        gavetaElement.classList.remove("-rigth-full");
+        gavetaElement.classList.add("rigth-0");
+    } else{
+        overlayElement.classList.add("invisible","opacity-0");
+        gavetaElement.classList.add("-right-full");
+        gavetaElement.classList.remove("right-0")
+    }
+    
+    
