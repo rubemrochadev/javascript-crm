5ae2a0dbf316c5cc3c097c0a4e427e4270de5edc
rubemrochadev <rubemrocha.dev@gmail.com>
2025-05-06 17:00:04 -0300

    cliente

--- clientes.js
+++ clientes.js
@@ -1,26 +1,9 @@
-let clientes = [
-    {
-        nome: "Conrado",
-        email: "conrado@gmail.com",
-        telefone: "(85) 00000-0000",
-        data: "05/05/2025"
-    },
-    {
-        nome: "Emanuel",
-        email: "emanuel@gmail.com",
-        telefone: "(85) 00000-0000",
-        data: "05/05/2025"
-    },
-    {
-        nome: "Ana",
-        email: "ana@gmail.com",
-        telefone: "(85) 00000-0000",
-        data: "05/05/2025"
-    },
-];
+let clientes = sessionStorage.getItem("clientes") ? JSON.parse(sessionStorage.getItem("clientes")) : [];
+
 
 function carregarClientes(listaDeClientes) {
     let tbodyElement = document.querySelector("#tabela");
+    tbodyElement.innerHTML = '';
     listaDeClientes.map((cliente) => {
         tbodyElement.innerHTML += `
         <tr class="*:leading-[40px]">
@@ -28,14 +11,29 @@ function carregarClientes(listaDeClientes) {
                         <td>${cliente.email}</td>
                         <td>${cliente.telefone}</td>
                         <td>${cliente.data}</td>
-                        <td class="w-[100] flex justify-center gap-4">
+                        <td class="w-[100px] flex justify-center gap-4">
                             <box-icon name="pencil"></box-icon>
                             <box-icon name="trash"></box-icon>
                         </td>
                     </tr>
                 `;
     })
-
 }
 
-carregarClientes(clientes)
+carregarClientes(clientes);
+
+function cadastrarCliente(form){
+    event.preventDefault();
+    // alert("Foi!!!")
+
+    //vão pegar os valores dos inputs do formulario e transformar em um objeto
+    let formData = new FormData(form);
+    let cliente = Object.fromEntries(formData.entries());
+    // console.log(dados);
+
+//inserir o novo cliente no final do array clientes
+    clientes.push(cliente);
+    sessionStorage.setItem("clientes",JSON.stringify(clientes));
+    mostrarOverlay();
+    carregarClientes(clientes);    
+}
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 
-function mostrarOverlay(){
+function mostrarOverlay() {
     let overlayElement = document.querySelector("#overlay");
     let gavetaElement = document.querySelector("#gaveta");
 
-    if(overlayElement.classList.contains("invisible")){
-        overlayElement.classList.remove("invisible","opacity-0");
+    if (overlayElement.classList.contains("invisible")) {
+        overlayElement.classList.remove("invisible", "opacity-0");
         gavetaElement.classList.remove("-right-full");
         gavetaElement.classList.add("right-0");
-    } else{
-        overlayElement.classList.add("invisible","opacity-0");
+    } else {
+        overlayElement.classList.add("invisible", "opacity-0");
         gavetaElement.classList.add("-right-full");
         gavetaElement.classList.remove("right-0");
     }
-    
+
 }
+
